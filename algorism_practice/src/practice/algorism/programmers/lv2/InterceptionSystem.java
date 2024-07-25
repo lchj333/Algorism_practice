95443a525f7f82f6576c234d8084f6f001a37278
chanju.lee <lchj333@ezens.net>
2024-07-25 14:22:31 +0900

    요격 시스템 - https://school.programmers.co.kr/learn/courses/30/lessons/181188
    (패키지 경로 이동)

---
+++ algorism_practice/src/practice/algorism/programmers/lv2/InterceptionSystem.java
@@ -0,0 +1,63 @@
+package practice.algorism.programmers.lv2;
+
+import java.util.Arrays;
+
+public class InterceptionSystem {
+
+    public static void main(String[] args) {
+        int[][] targets = {{4,5}, {4,8}, {10,14}, {11,13}, {5,12}, {3,7}, {1,4}};
+        new InterceptionSystem().solution(targets);
+    }
+
+    public int solution(int[][] targets) {
+        //int[a][b] : a 오름차순 정렬
+        Arrays.sort(targets, (o1, o2) -> o1[0] - o2[0]);
+
+        int answer = 0;
+        // 요격 미사일 방어 범위 초기화
+        int rangeMin = -1;
+        int rangeMax = -1;
+
+        for(int t = 0; t < targets.length; t++) {
+            int[] target = targets[t];
+            System.out.println("["+ target[0] + "][" + target[1] + "]");
+
+            if(rangeMin <= target[0] && rangeMax > target[0]) {
+                // 커버 가능 구간 & 범위 재 설정
+                rangeMin = Math.max(rangeMin, target[0]);
+                rangeMax = Math.min(rangeMax, target[1]);
+                System.out.println("rangeMin : " + rangeMin + ", rangeMax : " + rangeMax);
+            }else {
+                // 커버 불가능 -> 미사일 발사 추가
+                rangeMin = target[0];
+                rangeMax = target[1];
+                answer++;
+                System.out.println("answer++");
+            }
+        }
+
+        System.out.println("return " + answer);
+        return answer;
+    }
+}
+/*
+요격 시스템
+https://school.programmers.co.kr/learn/courses/30/lessons/181188
+
+문제 설명
+    A 나라가 B 나라를 침공하였습니다. B 나라의 대부분의 전략 자원은 아이기스 군사 기지에 집중되어 있기 때문에 A 나라는 B 나라의 아이기스 군사 기지에 융단폭격을 가했습니다.
+    A 나라의 공격에 대항하여 아이기스 군사 기지에서는 무수히 쏟아지는 폭격 미사일들을 요격하려고 합니다. 이곳에는 백발백중을 자랑하는 요격 시스템이 있지만 운용 비용이 상당하기 때문에 미사일을 최소로 사용해서 모든 폭격 미사일을 요격하려 합니다.
+    A 나라와 B 나라가 싸우고 있는 이 세계는 2 차원 공간으로 이루어져 있습니다. A 나라가 발사한 폭격 미사일은 x 축에 평행한 직선 형태의 모양이며 개구간을 나타내는 정수 쌍 (s, e) 형태로 표현됩니다.
+    B 나라는 특정 x 좌표에서 y 축에 수평이 되도록 미사일을 발사하며, 발사된 미사일은 해당 x 좌표에 걸쳐있는 모든 폭격 미사일을 관통하여 한 번에 요격할 수 있습니다.
+    단, 개구간 (s, e)로 표현되는 폭격 미사일은 s와 e에서 발사하는 요격 미사일로는 요격할 수 없습니다. 요격 미사일은 실수인 x 좌표에서도 발사할 수 있습니다.
+    각 폭격 미사일의 x 좌표 범위 목록 targets이 매개변수로 주어질 때, 모든 폭격 미사일을 요격하기 위해 필요한 요격 미사일 수의 최솟값을 return 하도록 solution 함수를 완성해 주세요.
+
+제한 사항
+    1 ≤ targets의 길이 ≤ 500,000
+    targets의 각 행은 [s,e] 형태입니다.
+    이는 한 폭격 미사일의 x 좌표 범위를 나타내며, 개구간 (s, e)에서 요격해야 합니다.
+    0 ≤ s < e ≤ 100,000,000
+입출력 예
+    targets	                                            result
+    [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]	3
+ */
