e33c9b99a9cd3ae50c7aa0ecd125e4d25a71a5d5
lchj333 <62678790+lchj333@users.noreply.github.com>
2024-09-19 09:34:17 +0900

    [level 1] Title: 최소직사각형, Time: 1.46 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,54 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int maxHorVal = 0;
-        int maxVerVal = 0;
-
+        int answer = 0;
+        int ver = 0;
+        int hor = 0;
+        
+        //NO 위치 변경
         for(int i = 0; i < sizes.length; i++) {
-            // sizes[i][0] 위치에 긴 모서리로 설정
-            if(sizes[i][0] < sizes[i][1]) {
-                int temp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = temp;
-            }
-
-            // 최대 가로, 세로 길이
-            if(maxHorVal < sizes[i][0])
-                maxHorVal = sizes[i][0];
-
-            if(maxVerVal < sizes[i][1])
-                maxVerVal = sizes[i][1];
+        	int[] temp = sizes[i];
+        	
+        	if(ver < temp[0]) {
+        		ver = temp[0];
+        	}
+        	
+        	if(hor < temp[1]) {
+        		hor = temp[1];
+        	}
         }
-
-        return maxHorVal * maxVerVal;
+        
+        answer = ver * hor;
+        ver = 0;
+        hor = 0;
+        
+        //YES 위치 변경
+        for(int j = 0; j < sizes.length; j++) {
+        	//돌리기
+        	int temp = sizes[j][0];
+        	sizes[j][0] = sizes[j][1];
+        	sizes[j][1] = temp;
+        	
+        	// 사이즈 찾기
+        	for(int i = 0; i < sizes.length; i++) {
+        		int[] size = sizes[i];
+        		
+        		if(ver < size[0]) {
+        			ver = size[0];
+        		}
+        		
+        		if(hor < size[1]) {
+        			hor = size[1];
+        		}
+        	}
+        	
+        	//원위치
+        	sizes[j][0] = sizes[j][1];
+        	sizes[j][1] = temp;
+        	
+        	//정답은?
+        	answer = Math.min(answer, ver*hor);
+        }
+        
+        return answer;
     }
 }
