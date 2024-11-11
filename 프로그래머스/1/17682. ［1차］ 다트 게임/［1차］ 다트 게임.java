85194edb337cdf88b84b764bac59587f35ffdda5
lchj333 <62678790+lchj333@users.noreply.github.com>
2024-11-11 12:17:45 +0900

    [level 1] Title: [1차] 다트 게임, Time: 12.39 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        char[] dart = dartResult.toCharArray();
+        int nowScore = 0;
+        int prevScore = 0;
+
+        int i = 0;
+        while(i < dart.length) {
+
+            if('0' <= dart[i] && dart[i] <= '9' ) {
+                // 점수 체크
+                if(dart[i] == '1' && dart[i + 1] == '0') {
+                    nowScore = Integer.parseInt(""+ dart[i] + dart[++i]);
+
+                }else {
+                    nowScore = Integer.parseInt(""+ dart[i]);
+                }
+                System.out.print("score : " + nowScore);
+
+            } else {
+                // 제곱 체크
+                int exp = switch (dart[i++]) {
+                    case 'S' -> 1;
+                    case 'D' -> 2;
+                    case 'T' -> 3;
+                    default -> 0;
+                };
+                System.out.print(", exp : " + exp);
+
+                // 제곱 계산.
+                int temp = nowScore;
+                for(; exp > 1; exp--) {
+                    temp *= nowScore;
+                }
+                nowScore = temp;
+                System.out.print(", now : " + nowScore + ", ");
+
+                // 옵션 체크
+                if(i < dart.length) {
+                    // 옵션 점수 계산
+                    switch (dart[i]) {
+                        case '*': // 스타상
+                            nowScore *= 2;
+                            break;
+                        case '#': // 아차상
+                            nowScore *= -1;
+                            prevScore = 0;
+                            break;
+                        default: // 옵션 없음
+                            prevScore = 0;
+                            i--; // no option -> 이미 증감된값 원위치
+                    }
+                    
+                    answer += prevScore + nowScore;
+                    prevScore = nowScore;
+                    System.out.println("answer : " + answer);
+
+                }else { // 옵션 무조건 없음
+                    // 점수 합산 normal
+                    answer += nowScore;
+                    System.out.println("answer : " + answer);
+                } // if end (option check)
+
+            } // if end (char check)
+
+            i++;
+        } // while end
+
+        return answer;
+    }
+}
